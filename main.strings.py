92b6be3ada7ad413461868a20fa4fa9461237b84
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2023-04-29 16:53:45 +0200

    Add files via upload

---
+++ main.strings.py
@@ -0,0 +1,21 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scorer_name ='\n', "Ruud Gullit"
+first_name = "Ruud"
+Last_name = "Gullit"
+when_they_scored= 32
+goal_0 = 32
+scorer_name ='\t' "Marco van Basten"
+when_they_scored = 54
+goal_1 = 54
+scorer_name = "scored in the <when_they_scored>nd minute"
+scorer_name = "scored in the <when_they_scored>th minute"
+print(" Ruud Gullit")
+print('when_they_scored')
+print('goal_0\ nd',32)
+print(" Marco van Basten")
+print('when_they_scored')
+print('goal_1\ th',54)
