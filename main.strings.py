dd05a9cbd5a4c618a5b9734d400a9a53c848a161
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2023-05-21 17:42:31 +0200

    Update main.strings.py
    update en correcties

--- main.strings.py
+++ main.strings.py
@@ -2,20 +2,36 @@
 __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
-# Add your code after this line
+# wincpy check strings
+
+# UEFA Euro 1988 Final
+
 scorer_name ='\n', "Ruud Gullit"
 first_name = "Ruud"
 Last_name = "Gullit"
-when_they_scored= 32
+when_they_scored = 32
 goal_0 = 32
-scorer_name ='\t' "Marco van Basten"
+scorer_name ='\t', "Marco van Basten"
 when_they_scored = 54
 goal_1 = 54
 scorer_name = "scored in the <when_they_scored>nd minute"
 scorer_name = "scored in the <when_they_scored>th minute"
-print(" Ruud Gullit")
+scorers = "Ruud Gullit 32, Marco van Basten 54"
+chant = "G.! G.!"
+name_short = "G. von Examplestein"
+good_chant = 2 != 3
+player = "G. von Examplestein"
+first_name = "G."
+last_name_len = 16
+report = "Ruud Gullit scored in the 32nd minute\nMarco van Basten scored in the 54th minute"
+print("Ruud Gullit")
 print('when_they_scored')
 print('goal_0\ nd',32)
-print(" Marco van Basten")
+print("Marco van Basten")
 print('when_they_scored')
 print('goal_1\ th',54)
+print(scorers, '\n', '\t')
+print(report,)
+print(name_short,)
+print(last_name_len,)
+print(2 != 3)
