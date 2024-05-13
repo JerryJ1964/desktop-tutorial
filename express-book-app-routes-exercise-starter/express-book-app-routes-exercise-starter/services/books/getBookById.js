72eef9bd1b6ab91bb326dc13425bde538e6ef782
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-05-13 15:08:47 +0200

    Delete express-book-app-routes-exercise-starter directory

--- express-book-app-routes-exercise-starter/express-book-app-routes-exercise-starter/services/books/getBookById.js
+++
@@ -1,7 +0,0 @@
-import bookData from '../../data/books.json' assert { type: 'json' };
-
-const getBookById = (id) => {
-  return bookData.books.find((book) => book.id === id);
-};
-
-export default getBookById;
--- express-book-app-routes-exercise-starter/express-book-app-routes-exercise-starter/services/records/getRecordById.js
+++
@@ -1,8 +0,0 @@
-import recordData from '../../data/records.json' assert { type: 'json' };
-
-const getRecordById = (id) => {
-  console.log(id);
-  return recordData.records.find((record) => record.id === id);
-};
-
-export default getRecordById;
