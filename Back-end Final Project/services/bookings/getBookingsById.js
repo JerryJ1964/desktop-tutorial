8172f4bba2f14c5ddda37083f88ed12b4c343a1d
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-05-24 16:10:50 +0200

    Delete Back-end Final Project directory

--- Back-end Final Project/services/bookings/getBookingsById.js
+++
@@ -1,8 +0,0 @@
-import bookingsData from '../../src/data/bookings.json' assert { type: 'json' }
-
-const getBookingById = (id) => {
-  console.log(id)
-  return bookingsData.records.find((booking) => booking.id === id)
-}
-
-export default getBookingById
--- Back-end Final Project/src/text/printUserData.js
+++
@@ -1,6 +0,0 @@
-import { fetchUser } from "../web/api.js";
-
-export async function printUserData(userId) {
-  const userData = await fetchUser(userId);
-  return `User: ${userData.name}, Email: ${userData.email}`;
-}
