97764f25de44676b15bbfd1738f5d32f642b893c
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-05-27 18:19:41 +0200

    Delete Back-end Final Project directory

--- Back-end Final Project/services/amenities/getAmenitiesById.js
+++
@@ -1,8 +0,0 @@
-import amenitiesData from '../../src/data/amenities.json' assert { type: 'json' }
-
-const getAmentiesById = (id) => {
-  console.log(id)
-  return amenitiesData.records.find((amenities) => amenities.id === id)
-}
-
-export default getAmentiesById
