3794c353ccf8c61c3c78df218c10255f85b843e6
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-05-29 15:58:55 +0200

    Delete Back-end Final Project directory

--- Back-end Final Project/services/reviews/getReviewsById.js
+++
@@ -1,8 +0,0 @@
-import reviewData from '../../src/data/reviews.json' assert { type: 'json' }
-
-const getReviewsById = (id) => {
-  console.log(id)
-  return reviewData.records.find((review) => review.id === id)
-}
-
-export default getReviewsById
