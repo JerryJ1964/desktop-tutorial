665b7adeb95ec3dacb158101443161927abb9e7e
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-06-03 16:56:15 +0200

    Delete Back-end Final Project directory

--- Back-end Final Project/services/bookings/getBookingsById.js
+++
@@ -1,8 +0,0 @@
-import bookingsData from '../../src/data/bookings.json' assert { type: 'json' }
-
-const getBookingsById = (id) => {
-  console.log(id)
-  return bookingsData.bookings.find((booking) => booking.id === id)
-}
-
-export default getBookingsById
