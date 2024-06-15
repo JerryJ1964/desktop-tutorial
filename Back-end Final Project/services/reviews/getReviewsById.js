be528251effc65ab1f1e618cd3d9a8117e7d7238
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-06-15 13:20:33 +0200

    Delete Back-end Final Project directory

--- Back-end Final Project/services/reviews/getReviewsById.js
+++
@@ -1,8 +0,0 @@
-import reviewsData from '../../src/data/reviews.json' assert { type: 'json' }
-
-const getReviewsById = (id) => {
-  console.log(id)
-  return reviewsData.reviews.find((review) => review.id === id)
-}
-
-export default getReviewsById
--- Back-end Final Project/services/users/getUsersById.js
+++
@@ -1,9 +0,0 @@
-import usersData from '../../src/data/users.json' assert { type: 'json' }
-
-
-const getUsersById = async (id) => {
-  console.log(id)
-  return usersData.users.find((user) => user.id === id)
-}
-
-export default getUsersById
