cc9be17d5122c73f0f329708bf496ead4f9b5462
JerryJ1964 <129662427+JerryJ1964@users.noreply.github.com>
2024-07-26 13:30:04 +0200

    Delete Back-end Final Project directory

--- Back-end Final Project/advancedAuth.js
+++
@@ -1,8 +0,0 @@
-import { auth } from "express-oauth2-jwt-bearer";
-
-const checkJwt = auth({
-  audience: process.env.API_IDENTIFIER || "{yourApiIdentifier}", // e.g. https://book-store-api
-  issuerBaseURL: process.env.ISSUER_BASE_URL || `https://{yourDomain}/`,
-});
-
-export default checkJwt;
